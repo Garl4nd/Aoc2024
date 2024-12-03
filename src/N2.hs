e745c1249346cf03929a4b30eb13932ec82c68e1
root <root@localhost.localdomain>
2024-12-03 00:01:54 +0000

    Solved #2:

--- src/N2.hs
+++ src/N2.hs
@@ -4,55 +4,36 @@ import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer as L
-
 import Control.Arrow
 import Control.Monad ((>=>))
 import Data.Either (fromRight)
-import Data.Function (on)
-import Data.List (sort)
 import Useful (countIf)
 
--- type TParser = Parsec Void T.Text
 type SParser = Parsec Void String
-
 parseFile :: String -> [[Int]]
 parseFile file =
   let
     lineParser :: SParser [Int]
     lineParser = sepBy L.decimal (char ' ')     
-    fileParser = sepEndBy lineParser newline
+    fileParser = sepBy lineParser newline
    in
-    fromRight [] $ runParser fileParser "" file
+    init $ fromRight [] $ runParser fileParser "" file
+
+(<||>) = liftA2 (||)
+--(<&&>) = liftA2 (&&)  
+
+isSafe :: [Int] -> Bool
+isSafe  = isSafeForward <||> (isSafeForward . reverse) where 
+    difInRange l r = (l+1<= r) && (r<=l+3) 
+    isSafeForward report = and $ zipWith  difInRange ( drop 1 report)  report        
 
 solution1 :: [[Int]] -> Int
-solution1 ls =
-  let    
-    between x l u  = (x>=l ) and (x<=r)
-    isSafeForward report = all $ zipWith ( \l r -> r - l `between` 1 3 )  report $ drop 1 report        
-    isSafe report = isSafeForward report or isSafeForward (reverse report) 
-   in
-    length $ filter isSafe ls
+solution1 = countIf isSafe
 
---    (lefts, rights) = join (***) sort $ unzip ls -- (map fst &&& map snd) ls
--- (lefts, rights) = (sort *** sort) $ unzip ls -- (map fst &&& map snd) ls
--- (lefts, rights) = uncurry ((,) `on` sort) $ unzip ls
-
--- sum $ zipWith ((abs .) . subtract) lefts rights
---  sum $ zipWith (subtract >>> (abs .)) lefts rights
--- sum $ zipWith (subtract >>> (>>>abs)) lefts rights
-
-solution2 :: [(Int, Int)] -> Int
-solution2 ls =
-  let
-    count target = countIf (== target)
-    (lefts, rights) = unzip ls
-   in
-    sum $ [l * count l rights | l <- lefts]
-
--- >>> print $ readFile "inputs/2.txt"
--- >>> parseFile <$> readFile "inputs/2.txt"
--- <stderr>: hPutChar: invalid argument (cannot encode character '\8216')
+solution2 :: [[Int]] -> Int
+solution2 = countIf (isSafe  <||> (any isSafe .  augmented)) where
+  augmented report = [remove n report | n <- [1..length report]]  
+  remove n report = let (l, r) = splitAt (n-1) report in l ++ drop 1 r
 
 getSolutions2 :: String -> IO (Int, Int)
-getSolutions2 = readFile >=> (parseFile >>> (solution1 &&& const 0) >>> return)
-  -- const $ return (0,0) --  readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
+getSolutions2 = readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
