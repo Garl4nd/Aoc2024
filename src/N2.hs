6d7e3e8608681d098cf26bc385bf1d54b7c1dc4f
root <root@localhost.localdomain>
2024-12-03 22:22:35 +0000

    Rewritten solution 2 in a more efficient way, and using foldr on the Maybe monad

--- src/N2.hs
+++ src/N2.hs
@@ -18,34 +18,36 @@ parseFile file =
     lineParser = sepBy L.decimal (char ' ')     
     fileParser = sepBy lineParser newline
    in
-    init $ fromRight [] $ runParser fileParser "" file
+   fromRight [] $ runParser fileParser "" file
 
 (<||>) = liftA2 (||)
 --(<&&>) = liftA2 (&&)  
 
 isSafe :: [Int] -> Bool
 isSafe  = isSafeForward <||> (isSafeForward . reverse) where 
-    difInRange l r = (l+1<= r) && (r<=l+3) 
+    difInRange l r = (l+1<= r) && (r<=l+3)
+    isSafeForward [] = False
     isSafeForward report = and $ zipWith  difInRange ( drop 1 report)  report        
-
-
 
 isSafe' :: Int -> [Int] -> Bool
 isSafe' lives = isSafeForward lives  <||> (isSafeForward lives . reverse) where
-  isSafeForward :: Int -> [Int]  -> Bool   
-  isSafeForward lives ls  = isJust $ foldr ( \cNum acc ->  case acc of 
-                                                        Nothing -> Nothing 
-                                                        Just (lastNum, lives) -> if inRange cNum lastNum then Just (cNum, lives) else  (if lives >1 then 
-                                                                                          Just (lastNum, lives -1) else Nothing)) (Just (last ls, lives)) $ init ls
+  isSafeForward :: Int -> [Int]  -> Bool  
+  isSafeForward _ [] = False
+  isSafeForward lives (x:xs)  = isJust $ foldr ((=<<).check)  (Just (x, lives)) $ reverse xs
+  check cNum (lastNum, lives) 
+    | inRange cNum lastNum = Just (cNum, lives) 
+    | lives >1 = Just (lastNum, lives -1) 
+    | otherwise = Nothing
   inRange l r = r>=l+1 && r<=l+3
+
 solution1 :: [[Int]] -> Int
-solution1 = countIf isSafe
+solution1 = countIf (isSafe' 1)
 
 solution2 :: [[Int]] -> Int
-solution2 = countIf (isSafe  <||> (any isSafe .  augmented)) where
-  augmented report = zipWith (++) (inits report) (tail $ tails report ) 
+solution2 = countIf (isSafe' 2) -- countIf (isSafe  <||> (any isSafe .  augmented)) where
+  --augmented report = zipWith (++) (inits report) (tail $ tails report ) 
   -- augmented report = [remove n report | n <- [1..length report]]  
   -- remove n report = let (l, r) = splitAt (n-1) report in l ++ drop 1 r
-
+-- \cNum acc -> check cNum =<< acc
 getSolutions2 :: String -> IO (Int, Int)
 getSolutions2 = readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
