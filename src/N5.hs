9b9fd65d6bf767cb493c1407b80bf9f3d6ce3f72
Filip Kostka, W <garland@DESKTOP-6D26JKA>
2024-12-05 22:23:02 +0100

    Added inputs/ folder (empty)

---
+++ src/N5.hs
@@ -0,0 +1,43 @@
+module N5 (getSolutions5) where
+
+import Control.Arrow
+import Control.Monad (void, (>=>))
+import Data.Either (fromRight)
+import Data.Maybe (catMaybes)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Debug
+
+type SParser = Parsec Void String
+
+exprParser1 :::: SParser [(Int, Int)]
+exprParser1 = catMaybes <$> manyTill (skipManyTill anySingle $ Nothing <$ try eof <|> Just <$> try mulParser) eof
+
+exprParser2 :: SParser [(Int, Int)]
+exprParser2 =
+  catMaybes
+    <$> manyTill
+      ( skipManyTill
+          anySingle
+          (Nothing <$ try ignoredInstructions <|> Nothing <$ try eof <|> Just <$> try mulParser)
+      )
+      eof
+ where
+  ignoredInstructions = string "don't()" >> skipManyTill anySingle (string "do()")
+
+mulParser :: SParser (Int, Int)
+mulParser = string "mul(" >> (,) <$> (L.decimal <* char ',') <*> (L.decimal <* char ')')
+
+parseFile1 :: String -> [(Int, Int)]
+parseFile1 file = fromRight [] $ runParser exprParser1 "" file
+
+parseFile2 :: String -> [(Int, Int)]
+parseFile2 file = fromRight [] $ runParser exprParser2 "" file
+
+solution :: [(Int, Int)] -> Int
+solution = sum . map (uncurry (*))
+
+getSolutions3 :: String -> IO (Int, Int)
+getSolutions3 = readFile >=> (((parseFile1 >>> solution) &&& (parseFile2 >>> solution)) >>> return)
