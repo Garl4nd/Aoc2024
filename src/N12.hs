4358de241662590a52380164b7253ffda39ab960
root <root@localhost.localdomain>
2024-12-13 00:08:35 +0000

    Solved #12

--- src/N12.hs
+++ src/N12.hs
@@ -1,62 +1,58 @@
-module N12 (getSolutions12)
-where
+module N12 (getSolutions12) where
 import Control.Arrow
 import Control.Monad ((>=>))
 import qualified Data.Array as A
 import qualified Data.Set as S
-import qualified Data.Map as M
-import Data.List (find)
 import  Data.Array ((!))
 
 import Useful (strToCharGrid, CharGrid, GridPos, countIf)
 import Data.Foldable (Foldable(toList))
 type PositionSet = S.Set GridPos
 
-type Crop = Char
 
 parseFile :: String -> CharGrid
 parseFile = strToCharGrid
 
 neighbors :: GridPos -> [GridPos]
-neighbors (y,x) = [(y+1,x), (y-1,x), (y, x-1), (y, x+1)]
+neighbors (y,x) = [(y,x-1), (y-1,x), (y, x+1), (y+1, x)]
 
-(<&&>) = liftA2 (&&)
-growRegion :: CharGrid -> GridPos -> (Crop, PositionSet)
-growRegion charGrid startPos = (val, go S.empty $ S.singleton startPos) where 
-  go :: PositionSet -> PositionSet -> PositionSet 
-  go currentSet boundary
-    | S.null boundary = currentSet
-    | otherwise = let newSet = S.union currentSet boundary
-                      newBoundary = S.unions $ S.map (S.fromList . filter ((`S.notMember` currentSet) <&&> inBounds <&&> isSameCrop) . neighbors ) boundary
-                  in go newSet newBoundary
+getRegion :: CharGrid -> GridPos -> PositionSet
+getRegion charGrid startPos = grow S.empty (S.singleton startPos) where 
+  grow :: PositionSet -> PositionSet -> PositionSet 
+  grow currentRegion boundary
+    | S.null boundary = currentRegion
+    | otherwise = let grownRegion = S.union currentRegion boundary
+                      newBoundary = S.unions $ S.map (S.fromList . filter ((`S.notMember` currentRegion) <&&> inBounds <&&> isSameCrop) . neighbors ) boundary
+                  in grow grownRegion newBoundary
   val = charGrid ! startPos 
-  inBounds = A.inRange bounds 
+  inBounds = A.inRange $ A.bounds charGrid 
   isSameCrop pos = charGrid ! pos == val
-  bounds = A.bounds charGrid
+  (<&&>) = liftA2 (&&)
 
-classifyPlots :: CharGrid -> M.Map Crop [PositionSet]
-classifyPlots charGrid = go M.empty where
-  indices = A.indices charGrid
-  go :: M.Map Crop [PositionSet] -> M.Map Crop [PositionSet]
-  go currentMap
-    | Just nextStart <- findUnassigned = let 
-      (crop, posSet) = growRegion charGrid nextStart 
-      in go $ M.insertWith (++) crop [posSet]  currentMap 
-    | otherwise = currentMap where  
-    findUnassigned = find (`S.notMember` assigned ) indices 
-    assigned = S.unions $ concat $ M.elems currentMap
+getAllRegions :: CharGrid ->  [PositionSet]
+getAllRegions charGrid = go [] $ S.fromList  (A.indices charGrid) where
+  go :: [PositionSet] -> PositionSet -> [PositionSet]
+  go foundRegions unassignedSet
+    | S.null unassignedSet = foundRegions
+    | otherwise = let newRegion = getRegion charGrid (S.elemAt 0 unassignedSet) 
+                      newUnassignedSet = S.difference unassignedSet newRegion in
+                        go (newRegion : foundRegions) newUnassignedSet 
 
-allPlotGroups = concat . M.elems . classifyPlots
+perimeter :: PositionSet ->  Int 
+perimeter posSet = sum $  countIf (`S.notMember` posSet) . neighbors  <$> toList posSet
 
-areaPerim :: PositionSet -> (Int, Int) 
-areaPerim posSet = (area, perimeter) where 
-  area = length posSet 
-  perimeter = sum $  length . filter (`S.notMember` posSet) . neighbors  <$> toList posSet
+numOfSides :: PositionSet -> Int
+numOfSides posSet = sum $ numCorners <$>  toList posSet where
+ numCorners  (y,x) = countIf  
+  (\(adj1, adj2, corner) -> 
+  (adj1 `S.notMember` posSet && adj2 `S.notMember` posSet) || (adj1 `S.member` posSet && adj2 `S.member` posSet && corner `S.notMember` posSet)) touchingNeighbors  where
+  touchingNeighbors = [((y+dy, x), (y, x+dx), (y+dy, x+dx)) | dy <- [-1, 1], dx <- [-1,1]]
 
 solution1 :: CharGrid -> Int 
-solution1 charGrid = let 
- f = map (uncurry (*) . areaPerim) $  concat $ M.elems (classifyPlots charGrid)  
- in sum f
+solution1 charGrid = sum $ liftA2 (*)  length perimeter  <$> getAllRegions charGrid   
+
+solution2 :: CharGrid -> Int 
+solution2 charGrid = sum $ liftA2 (*) length numOfSides  <$> getAllRegions charGrid   
 
 getSolutions12 :: String -> IO (Int, Int)
-getSolutions12 = const $ return (0,0)-- readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
+getSolutions12 = readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
