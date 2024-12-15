576cff347ff628509bc42485a24806959f6d72f9
Filip Kostka, W <garland@DESKTOP-6D26JKA>
2024-12-15 21:49:17 +0100

    Civilised the atrocity

--- src/N15.hs
+++ src/N15.hs
@@ -1,23 +1,21 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module N15 () where
+module N15 (getSolutions15) where
 
-import Control.Monad (foldM, guard, unless, when)
+import Control.Monad (unless)
 import Control.Monad.ST (ST, runST)
 import Control.Monad.Trans.Class (MonadTrans (lift))
 import Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
-import Control.Monad.Trans.State (State, get, put)
 import Data.Array.Base (MArray (getBounds), STUArray, freezeSTUArray, modifyArray, readArray, thawSTUArray, writeArray)
-import Data.Array.Unboxed (indices, (!))
+import Data.Array.Unboxed ((!))
 import qualified Data.Array.Unboxed as A
 import Data.List (find, findIndex, inits)
 import Data.Maybe (catMaybes, fromJust, mapMaybe)
 import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
-import Debugging (traceWInfo)
 import Useful (CharGridU, GridPos, appendGridToFile, charGridToStr, saveGridToFile, splitBySubstr, strToCharGrid)
 
 type STCharGrid s = STUArray s GridPos Char
-
+type RobotMover s = ReaderT (STCharGrid s, STRef s GridPos) (ST s)
 data Direction = L | U | R | D deriving (Eq, Show)
 
 -- type Direction = GridPos -> GridPos
@@ -34,74 +32,99 @@ up (y, x) = (y - 1, x)
 down (y, x) = (y + 1, x)
 
 moveDir :: Direction -> GridPos -> GridPos
-moveDir U (y, x) = (y - 1, x)
-moveDir D (y, x) = (y + 1, x)
-moveDir L (y, x) = (y, x - 1)
-moveDir R (y, x) = (y, x + 1)
-
-trace :: (Show a) => String -> a -> a
-trace = traceWInfo False
-
-type RobotMover s = ReaderT (STCharGrid s, STRef s GridPos) (ST s) ()
+moveDir U = up
+moveDir D = down
+moveDir L = left
+moveDir R = right
 
 parseFile :: String -> (CharGridU, [Direction], GridPos)
 parseFile file =
   let
-    [gridStr, directions] = trace "splits" $ splitBySubstr "\n\n" (trace "f" file)
+    [gridStr, directions] = splitBySubstr "\n\n" file
     gridAr = strToCharGrid gridStr
-    initPos = fromJust $ find (\pos -> gridAr ! pos == '@') $ indices gridAr
+    initPos = fromJust $ find (\pos -> gridAr ! pos == '@') $ A.indices gridAr
    in
     (gridAr, mapMaybe charToDir directions, initPos)
 
-animateRobot :: [Direction] -> RobotMover s
+animateRobot :: [Direction] -> RobotMover s ()
 animateRobot [] = return ()
 animateRobot (currentInstruction : remDirections) = do
-  moveRobot2 currentInstruction
+  moveRobotAndCrates currentInstruction
   animateRobot remDirections
 
-moveRobot :: forall s. Direction -> RobotMover s
-moveRobot d = do
-  let dir = moveDir d
+moveRobotAndCrates :: forall s. Direction -> RobotMover s ()
+moveRobotAndCrates dir = do
+  let move = moveDir dir
   (ar, currentPosRef) <- ask
   currentPos <- lift $ readSTRef currentPosRef
+  let movePos = move currentPos
   bounds <- lift $ getBounds ar
-  let movePos = dir currentPos
   unless (A.inRange bounds movePos) $ return ()
   moveVal <- lift $ readArray ar movePos
-  let moveCurrent = do
-        writeArray ar currentPos '.'
-        writeArray ar movePos '@'
-        writeSTRef currentPosRef movePos
-  let moveableList :: GridPos -> [GridPos] -> ST s (Maybe [GridPos])
-      moveableList pos ls =
-        if not $ A.inRange bounds pos
-          then return Nothing
-          else do
-            val <- ar `readArray` pos
-            case val of
-              '#' -> return Nothing
-              '.' -> return $ Just (pos : ls)
-              'O' -> moveableList (dir pos) (pos : ls)
-
-  -- _ -> return Nothing
   case moveVal of
     '#' -> return ()
     _ -> do
-      maybeMoves <- lift $ moveableList movePos []
+      maybeMoves <- moveableCrates movePos dir
       case maybeMoves of
         Nothing -> return ()
-        Just moves -> lift $ do
-          mapM_ (\pos -> writeArray ar pos 'O') moves
-          moveCurrent
+        Just moves -> do
+          lift $ do
+            vals <- mapM (readArray ar) moves
+            mapM_ (\pos -> writeArray ar pos '.') $ reverse moves
+            mapM_ (\(pos, val) -> writeArray ar (move pos) val) $ zip moves vals
+          moveRobot movePos
 
-duplicateInput :: String -> String
-duplicateInput [] = []
-duplicateInput (c : rest)
-  | c == '.' = '.' : '.' : duplicateInput rest
-  | c == '#' = '#' : '#' : duplicateInput rest
-  | c == 'O' = '[' : ']' : duplicateInput rest
-  | c == '@' = '@' : '.' : duplicateInput rest
-  | otherwise = c : duplicateInput rest
+moveableCrates :: GridPos -> Direction -> RobotMover s (Maybe [GridPos])
+moveableCrates pos dir = do
+  (ar, _) <- ask
+  bounds <- lift $ getBounds ar
+  let move = moveDir dir
+  if not $ A.inRange bounds pos
+    then return Nothing
+    else do
+      val <- lift $ ar `readArray` pos
+      case val of
+        '#' -> return Nothing
+        '.' -> return $ Just []
+        'O' -> do
+          maybeLs <- moveableCrates (move pos) dir
+          case maybeLs of
+            Just ls -> return $ Just (pos : ls)
+            _ -> return Nothing
+        _ ->
+          if dir `elem` [L, R]
+            then do
+              let rightPos = move pos
+              maybeLs <- moveableCrates (move rightPos) dir
+              case maybeLs of
+                Just ls -> return $ Just (pos : rightPos : ls)
+                _ -> return Nothing
+            else do
+              let otherPos = if val == '[' then right pos else left pos
+
+              maybeLs1 <- moveableCrates (move pos) dir
+              maybeLs2 <- moveableCrates (move otherPos) dir
+              case (maybeLs1, maybeLs2) of
+                (Just ls1, Just ls2) -> return $ Just (pos : otherPos : ls1 ++ ls2)
+                _ -> return Nothing
+
+moveRobot :: GridPos -> RobotMover s ()
+moveRobot movePos = do
+  (ar, currentPosRef) <- ask
+  lift $ do
+    currentPos <- readSTRef currentPosRef
+    writeArray ar currentPos '.'
+    writeArray ar movePos '@'
+    writeSTRef currentPosRef movePos
+
+complicateInput :: String -> String
+complicateInput [] = []
+complicateInput (c : rest)
+  | c == '.' = ".." ++ complicateInput rest
+  | c == '#' = "##" ++ complicateInput rest
+  | c == 'O' = "[]" ++ complicateInput rest
+  | c == '@' = "@." ++ complicateInput rest
+  | otherwise = c : complicateInput rest
 evolveAr :: (CharGridU, [Direction], GridPos) -> CharGridU
 evolveAr (ar, directions, initPos) = runST $ do
   star <- thawSTUArray ar
@@ -109,18 +132,19 @@ evolveAr (ar, directions, initPos) = runST $ do
   runReaderT (animateRobot directions) (star, pos)
   freezeSTUArray star
 
-gpsCoordinateSum :: CharGridU -> Int
-gpsCoordinateSum charGrid =
-  let coords = filter (\pos -> charGrid ! pos == 'O') $ A.indices charGrid
+gpsCoordinateSum :: Char -> CharGridU -> Int
+gpsCoordinateSum symbol charGrid =
+  let coords = filter (\pos -> charGrid ! pos == symbol) $ A.indices charGrid
    in sum $ map (\(y, x) -> 100 * (y - 1) + x - 1) coords
+solution1 = gpsCoordinateSum 'O' . evolveAr
+solution2 = gpsCoordinateSum '[' . evolveAr
 
-gpsCoordinateSum2 :: CharGridU -> Int
-gpsCoordinateSum2 charGrid =
-  let coords = filter (\pos -> charGrid ! pos == '[') $ A.indices charGrid
-   in sum $ map (\(y, x) -> 100 * (y - 1) + x - 1) coords
-
-solution1 = gpsCoordinateSum . evolveAr . parseFile
-solution2 = gpsCoordinateSum2 . evolveAr . parseFile . duplicateInput
+getSolutions15 :: String -> IO (Int, Int)
+getSolutions15 filename =
+  do
+    input <- readFile filename
+    let complicatedInput = complicateInput input
+    return (solution1 . parseFile $ input, solution2 . parseFile $ complicatedInput)
 
 test :: String -> IO ()
 test inputFile = do
@@ -132,60 +156,8 @@ test inputFile = do
 
 test2 :: String -> IO ()
 test2 inputFile = do
-  (ar, dirs, initPos) <- (parseFile . duplicateInput) <$> readFile inputFile
+  (ar, dirs, initPos) <- (parseFile . complicateInput) <$> readFile inputFile
   saveGridToFile "outputs/origAr2.txt" ar
   let modFile = "outputs/modar2.txt"
   writeFile modFile ""
   mapM_ (\dirs -> appendGridToFile modFile (evolveAr (ar, dirs, initPos)) >> appendFile modFile (("\n" <> if null dirs then "" else (show $ last dirs)) <> "\n")) $ inits dirs
-
-moveRobot2 :: forall s. Direction -> RobotMover s
-moveRobot2 d = do
-  let dir = moveDir d
-  (ar, currentPosRef) <- ask
-  currentPos <- lift $ readSTRef currentPosRef
-  bounds <- lift $ getBounds ar
-  let movePos = dir currentPos
-  unless (A.inRange bounds movePos) $ return ()
-  moveVal <- lift $ readArray ar movePos
-  let moveCurrent = do
-        writeArray ar currentPos '.'
-        writeArray ar movePos '@'
-        writeSTRef currentPosRef movePos
-  let moveableList :: GridPos -> ST s (Maybe [GridPos])
-      moveableList pos =
-        if not $ A.inRange bounds pos
-          then return Nothing
-          else do
-            val <- ar `readArray` (trace "pos" pos)
-            case trace "val" val of
-              '#' -> return Nothing
-              '.' -> return $ Just []
-              _ ->
-                if
-                  | d `elem` [L, R] -> do
-                      let rightPos = dir pos
-                      maybeLs <- moveableList (dir rightPos)
-                      case maybeLs of
-                        Just ls -> return $ Just (pos : rightPos : ls)
-                        _ -> return Nothing
-                  | otherwise -> do
-                      let otherPos = if val == '[' then right pos else left pos
-
-                      maybeLs1 <- moveableList (dir pos)
-                      maybeLs2 <- moveableList (dir $ otherPos)
-                      case (maybeLs1, maybeLs2) of
-                        (Just ls1, Just ls2) -> return $ Just (pos : otherPos : ls1 ++ ls2)
-                        _ -> return Nothing
-
-  -- _ -> return Nothing
-  case moveVal of
-    '#' -> return ()
-    _ -> do
-      maybeMoves <- lift $ moveableList movePos
-      case maybeMoves of
-        Nothing -> return ()
-        Just moves -> lift $ do
-          vals <- mapM (\pos -> readArray ar pos) moves
-          mapM_ (\pos -> writeArray ar pos '.') $ reverse $ trace "moves" moves
-          mapM_ (\(pos, val) -> writeArray ar (dir pos) val) $ zip moves vals
-          moveCurrent
