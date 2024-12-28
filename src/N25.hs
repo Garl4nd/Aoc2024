f3d16ea5ae7c5d2eb95c5f28b825aacd6ef52a90
Filip Kostka, W <garland@DESKTOP-6D26JKA>
2024-12-28 15:36:47 +0100

    Finishing touches

--- src/N25.hs
+++ src/N25.hs
@@ -1,40 +1,36 @@
 module N25 (getSolutions25) where
 
 import Control.Arrow
-import Control.Monad ((>=>))
-import qualified Data.Array as A
+import Control.Monad (join, (>=>))
 import Data.List (partition, transpose)
-import Useful
 
 type ListGrid = [[Char]]
 type CondensedGrid = [Int]
+data GridType = Key | Lock
+
 parseFile :: String -> ([CondensedGrid], [CondensedGrid])
-parseFile file = (map (condense Lock) *** map (condense Key)) . classify . go $ lines file
+parseFile file = (map (condense Lock) *** map (condense Key)) . partition isLock . go $ lines file
  where
   go :: [String] -> [ListGrid]
   go [] = []
-  go lines =
-    let (thisPart, rest) = splitAt 8 lines
-     in (take 7 $ thisPart) : go rest
-
-classify :: [ListGrid] -> ([ListGrid], [ListGrid])
-classify = partition isLock
- where
+  go lns =
+    let (currentGrid, rest) = splitAt 8 lns
+     in take 7 currentGrid : go rest
   isLock = all (== '#') . head
-
-data GridType = Key | Lock
-condense :: GridType -> ListGrid -> [Int]
-condense Lock grid = [length . takeWhile (== '#') $ col | col <- transpose grid]
-condense Key grid = [length . takeWhile (== '#') $ col | col <- transpose . reverse $ grid]
+  condense :: GridType -> ListGrid -> [Int]
+  condense gridType grid = [length . takeWhile (== '#') $ col | col <- transpose grid']
+   where
+    grid' = case gridType of
+      Lock -> grid
+      Key -> reverse grid
 
 fits :: CondensedGrid -> CondensedGrid -> Bool
 fits lock key = and $ zipWith (\l k -> l + k <= 7) lock key
 
-solution :: ([CondensedGrid], [CondensedGrid]) -> Int
-solution (locks, keys) = length [[] | lock <- locks, key <- keys, fits lock key]
+solution1 :: ([CondensedGrid], [CondensedGrid]) -> Int
+solution1 (locks, keys) = length [() | lock <- locks, key <- keys, fits lock key]
+
+solution2 = const $ 2024 * 50
 
 getSolutions25 :: String -> IO (Int, Int)
-getSolutions25 = readFile >=> (parseFile >>> (solution &&& (const $ 50 * 24)) >>> return)
-
--- >>> parseFile <$> readFile "inputs/25.txt"
--- getLinkDeps: Home module not loaded Useful main-9d09fbcc05f48f36b790b3f194f6b63b4d88732d
+getSolutions25 = readFile >=> (parseFile >>> (solution1 &&& solution2) >>> return)
